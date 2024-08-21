ba7608535cbbea3d25e9dd0af8021a6f3c0e7d4f
oshimmmm <yasuhirooshima0419@gmail.com>
2024-08-22 06:47:51 +0900

    firstcommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,11 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/pages/index.tsx</code>
         </p>
+        <h1>
+          <Link href="/posts/firstpost" legacyBehavior>
+            <a>firstpost!</a>
+          </Link>
+        </h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
